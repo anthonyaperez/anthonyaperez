b87f70f170a65cfb343667ea710f03fb9f282623
Anthony Perez <anthonyacostaperez@gmail.com>
2019-09-11 00:45:34 -0500

    1

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -20,7 +20,7 @@ export function signUserIn(data) {
             .then(res => {
                 dispatch({type: AUTH_USER})
                 localStorage.setItem('auth_jwt_token', res.data.token);
-                window.location = '/#account';
+                window.location = 'anthonyaperez/#account';
                 axios.defaults.headers.common['Authorization'] = localStorage.getItem('auth_jwt_token');
             })
             .catch(error => {
@@ -38,7 +38,7 @@ export function signUserUp(userObj) {
             .then(res => {
                 dispatch({type: AUTH_USER})
                 localStorage.setItem('auth_jwt_token', res.data.token);
-                window.location = '/#account';
+                window.location = 'anthonyaperez/#account';
                 axios.defaults.headers.common['Authorization'] = localStorage.getItem('auth_jwt_token');
             })
             .catch(error => {

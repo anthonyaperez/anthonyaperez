92ad473c48cafe4b3928352735153c95603f8627
Anthony Perez <anthonyacostaperez@gmail.com>
2019-09-10 23:39:05 -0500

    test 3

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production'),
-                'API_URI': JSON.stringify('https://warm-bayou-86769.herokuapp.com')
+                'API_URI': JSON.stringify('https://anthonyaperez.herokuapp.com')
             },
         }),
     ]
